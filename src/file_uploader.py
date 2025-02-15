ed64c576a829fc403347e135d091b7bddb7b7423
rtarasenko <rtarasenko@at_consulting.ru>
2025-02-15 19:29:19 +0300

    Add docstrings and refactor methods in FileUploader for better clarity

--- src/file_uploader.py
+++ src/file_uploader.py
@@ -1,42 +1,56 @@
+"""
+Модуль для обработки загрузки файлов.
+
+Этот модуль содержит класс `FileUploader`, который предоставляет методы для 
+сохранения загруженных файлов на диск. Он использует Flask для обработки 
+HTTP-запросов и обеспечивает создание необходимой директории для сохранения 
+файлов, если она не существует.
+
+Класс:
+    FileUploader: Класс, который управляет загрузкой и сохранением файлов.
+"""
 import os
 
-from flask import request, jsonify
+from flask import request
 
-
-def get_filename(file):
-    return file.filename.split(".")[0]
-
-
-def ensure_folder_exists(folder):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-
-def create_error_response(message, status_code):
-    return jsonify({"error": message}), status_code
-
+from src.common import ensure_folder_exists
 
 class FileUploader:
+    """
+    Класс для обработки загрузки файлов.
+
+    Этот класс предоставляет методы для сохранения загруженных файлов на диск. 
+    Он использует Flask для обработки HTTP-запросов и обеспечивает создание 
+    необходимой директории для сохранения файлов, если она не существует.
+
+    :param upload_folder (str): Путь к директории, в которую будут сохраняться загруженные файлы.
+    """
     def __init__(self, upload_folder='../uploads'):
         self.upload_folder = upload_folder
         ensure_folder_exists(self.upload_folder)
 
     def save_file(self):
         """
-        Сохранение файла
+        Обрабатывает загруженный файл и сохраняет его на диск.
+
+        Метод извлекает файл, переданный через HTTP-запрос, и передает его в метод `save_file_to_disk` для сохранения.
+
+        :return: Путь к сохраненному файлу, если файл был передан; в противном случае возвращается None.
         """
         file = request.files.get('file')
-        if file is None:
-            return create_error_response("No file part", 400)
+        if file:
+            return self.save_file_to_disk(file)
+        else:
+            return None
 
-        filename = get_filename(file)
-        if filename is None:
-            return create_error_response("No selected file", 400)
+    def save_file_to_disk(self, file) -> str:
+        """
+        Сохраняет загруженный файл на диск в указанную директорию.
 
-        saved_file_path = self.save_file_to_disk(file, filename)
-        return [filename, saved_file_path], 200
-
-    def save_file_to_disk(self, file, filename):
+        :param file: объект файла, переданный через HTTP-запрос.
+        :return: путь к сохраненному файлу.
+        """
+        filename = file.filename.split(".")[0]
         file_path = os.path.join(self.upload_folder, filename)
         file.save(file_path)
         return file_path
